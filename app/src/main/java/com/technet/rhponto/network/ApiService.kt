b4cb189a5cfcebf8858b7953d65bbc25f245bf15
jeffersoncaarvalho12-design <jeffersoncaarvalho12@gmail.com>
2026-04-24 18:11:04 -0300

    Add primeiroAcesso function to ApiService

--- app/src/main/java/com/technet/rhponto/network/ApiService.kt
+++ app/src/main/java/com/technet/rhponto/network/ApiService.kt
@@ -3,12 +3,16 @@ package com.technet.rhponto.network
 import com.technet.rhponto.model.BatidaResponse
 import com.technet.rhponto.model.HistoricoResponse
 import com.technet.rhponto.model.LoginResponse
+import com.technet.rhponto.model.PrimeiroAcessoResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("login.php")
     suspend fun login(@Body body: Map<String, String>): LoginResponse
+
+    @POST("primeiro_acesso.php")
+    suspend fun primeiroAcesso(@Body body: Map<String, String>): PrimeiroAcessoResponse
 
     @POST("bater_ponto.php")
     suspend fun baterPonto(@Body body: Map<String, @JvmSuppressWildcards Any?>): BatidaResponse
