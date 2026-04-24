8472e7b2d8ec1ce85200851271a0ae52c89773c0
jeffersoncaarvalho12-design <jeffersoncaarvalho12@gmail.com>
2026-04-24 19:17:52 -0300

    Refactor ApiService to use specific request models

--- app/src/main/java/com/technet/rhponto/network/ApiService.kt
+++ app/src/main/java/com/technet/rhponto/network/ApiService.kt
@@ -1,22 +1,26 @@
 package com.technet.rhponto.network
 
+import com.technet.rhponto.model.BaterPontoRequest
 import com.technet.rhponto.model.BatidaResponse
+import com.technet.rhponto.model.HistoricoRequest
 import com.technet.rhponto.model.HistoricoResponse
+import com.technet.rhponto.model.LoginRequest
 import com.technet.rhponto.model.LoginResponse
+import com.technet.rhponto.model.PrimeiroAcessoRequest
 import com.technet.rhponto.model.PrimeiroAcessoResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("login.php")
-    suspend fun login(@Body body: Map<String, String>): LoginResponse
+    suspend fun login(@Body body: LoginRequest): LoginResponse
 
     @POST("primeiro_acesso.php")
-    suspend fun primeiroAcesso(@Body body: Map<String, String>): PrimeiroAcessoResponse
+    suspend fun primeiroAcesso(@Body body: PrimeiroAcessoRequest): PrimeiroAcessoResponse
 
     @POST("bater_ponto.php")
-    suspend fun baterPonto(@Body body: Map<String, @JvmSuppressWildcards Any?>): BatidaResponse
+    suspend fun baterPonto(@Body body: BaterPontoRequest): BatidaResponse
 
     @POST("meu_historico.php")
-    suspend fun historico(@Body body: Map<String, @JvmSuppressWildcards Any?>): HistoricoResponse
+    suspend fun historico(@Body body: HistoricoRequest): HistoricoResponse
 }
